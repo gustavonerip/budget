7b35fc6204105f6d4f10d6c2ba6ae37ea63cf3ad
Gustavo <gustavoneripeixoto@gmail.com>
2023-05-11 13:35:40 -0700

    event listeners added to budget.js

--- budget.js
+++ budget.js
@@ -1,1 +1,56 @@
 // Select elements
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl  = document.querySelector(".income p");
+const outcomeTotalEl = document.querySelector(".outcome p");
+const incomeEl = document.querySelector("#income");
+const expenseEl = document.querySelector("#expense");
+const allEl = document.querySelector("#all");
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#expense .list");
+const allList = document.querySelector("#all .list");
+
+// Select buttons
+const allBtn = document.querySelector("#tab-all");
+const incomeBtn = document.querySelector("#tab-income");
+const expenseBtn = document.querySelector("#tab-expense");
+
+//  Select inputs
+
+const incomeTitle = document.querySelector("#income-desc-input");
+const incomeAmount = document.querySelector("#income-amount-input");
+const addIncome = document.querySelector("#add-income");
+
+const expenseTitle = document.querySelector("#expense-desc-input"); 
+const expenseAmount = document.querySelector("#expense-amount-input"); 
+const addExpense = document.querySelector("#add-expense"); 
+
+// Variables
+let ENTRY_LIST = [];
+let balance = 0, income = 0, outcome = 0; 
+
+const DELETE = "delete", EDIT = "edit";
+
+// Event listeners
+incomeBtn.addEventListener('click', () => {
+    show(incomeEl);
+    hide([expenseEl,allEl]);
+    active(incomeBtn);
+    inactive([expenseBtn,allBtn]);
+});
+expenseBtn.addEventListener('click', () => {
+    show(expenseEl);
+    hide([incomeEl,allEl]);
+    active(expenseBtn);
+    inactive([incomeBtn,allBtn]);
+});
+allBtn.addEventListener('click', () => {
+    show(allEl);
+    hide([incomeEl,expenseEl]);
+    active(allBtn);
+    inactive([incomeBtn,expenseBtn]);
+});
+
+// Functions
+function show(element){
+    
+}
